19f6ad2f789309660b7d2160c7f58e772cd1f04e
PeanutbutterWarrior <65+PeanutbutterWarrior@users.noreply.github.com>
2021-02-22 14:28:14 +0000

    Handle multiple clients

--- Server.py
+++ Server.py
@@ -5,20 +5,28 @@ import threading
 def manage_client(conn, addr):
     with conn:
         while True:
-            data = conn.recv(1024)
-            if not data:
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f'Received {data} from {addr[0]}:{addr[1]}')
+            except ConnectionResetError:
+                print(f'{addr[0]}:{addr[1]} disconnected')
                 break
-            print(f'Received {data} from {addr[0]}:{addr[1]}')
 
 
 HOST = 'localhost'
 PORT = 56789
 
 users = {}
+threads = []
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    connection, address = s.accept()
-    print(f'Recieved connection from {address[0]}:{address[1]}')
-    manage_client(connection, address)
+    while True:
+        connection, address = s.accept()
+        print(f'Recieved connection from {address[0]}:{address[1]}')
+        client_thread = threading.Thread(target=manage_client, args=(connection, address), daemon=True)
+        threads.append(client_thread)
+        client_thread.start()

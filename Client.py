720c648d88ff6194a43f31224abf432a3ba8d7ce
PeanutbutterWarrior <65+PeanutbutterWarrior@users.noreply.github.com>
2021-02-22 14:19:12 +0000

    Allow sending multiple messages

--- Client.py
+++ Client.py
@@ -1,8 +1,11 @@
 import socket
+import time
 
 HOST = 'localhost'
 PORT = 56789
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello')
+    while True:
+        message = input('message: ')
+        s.sendall(message.encode('utf-8'))

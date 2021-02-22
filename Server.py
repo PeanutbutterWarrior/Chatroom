e3d638c8fc690999b5446aef87217cafa1068680
PeanutbutterWarrior <65+PeanutbutterWarrior@users.noreply.github.com>
2021-02-22 15:28:08 +0000

    Add joining and leaving messages

--- Server.py
+++ Server.py
@@ -4,8 +4,10 @@ import threading
 
 def manage_client(conn, iden):
     username = conn.recv(1024).decode('utf-8')
-    users[iden]['username'] = username
+    users[iden]['name'] = username
+    print(f'{iden} is named {username}')
     users[iden]['ready'] = True
+    disseminate_message(iden, f'{users[iden]["name"]} connected'.encode('utf-8'), prefix_username=False)
     with conn:
         while True:
             try:
@@ -16,11 +18,20 @@ def manage_client(conn, iden):
                 disseminate_message(iden, data)
             except ConnectionResetError:
                 print(f'{iden} disconnected')
+                disseminate_message(
+                                    iden,
+                                    f'{users[iden]["name"]} disconnected'.encode('utf-8'),
+                                    prefix_username=False
+                                    )
+                del users[iden]
                 break
 
 
-def disseminate_message(origin, message):
-    to_send = f'{users[origin]["username"]}: {message.decode("utf-8")}'.encode('utf-8')
+def disseminate_message(origin, message, prefix_username=True):
+    if prefix_username:
+        to_send = f'{users[origin]["name"]}: {message.decode("utf-8")}'.encode('utf-8')
+    else:
+        to_send = message
     for iden, data in users.items():
         if iden != origin and data['ready']:
             data['connection'].sendall(to_send)

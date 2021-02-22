afeb93518d2e2039958336475247aa613cfe0339
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2021-02-22 22:44:21 +0000

    Change communication to be json and add logins

--- Client.py
+++ Client.py
@@ -1,27 +1,108 @@
 import socket
 import threading
+import json
+import hashlib
+
+
+# Characters allowed in usernames and passwords
+allowed_chars = set('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_=+')
 
 
 def send_messages(connection):
     while True:
         message = input()
-        connection.sendall(message.encode('utf-8'))
+        data = json.dumps({'action': 'send', 'text': message})
+        connection.sendall(data.encode('utf-8'))
+
+
+def check_username(username):
+    if len(username) > 16:
+        return False
+    for char in username:
+        if char not in allowed_chars:
+            return False
+    return True
+
+
+def check_password(password):
+    if len(password) > 32:
+        return False
+    for char in password:
+        if char not in allowed_chars:
+            return False
+    return True
+
+
+def get_username(new_user):
+    while True:
+        username = input('Username: ')
+        if not new_user or check_username(username):
+            break
+        print('Invalid username. Allowed characters are letters, numbers, -, _, = and +. '
+              'No diacritics are allowed. It cannot be more than 16 characters long')
+    return username
+
+
+def get_password(new_user):
+    while True:
+        password = input('Password: ')
+        if not new_user or check_password(password):
+            break
+        print('Invalid password. Allowed characters are letters, numbers, -, _, = and +. '
+              'No diacritics are allowed. It cannot be more than 32 characters long')
+
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return password
 
 
 HOST = 'localhost'
 PORT = 56789
 
-username = input('Username: ')
+
+new_user = True if input('Do you have an account (y/n)? ') == 'n' else False
+username = get_username(new_user)
+password = get_password(new_user)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(username.encode('utf-8'))
+
+    # Either create a login for a user or log them in
+    if new_user:
+        while True:
+            s.sendall(json.dumps({'action': 'register', 'username': username, 'password': password}).encode('utf-8'))
+            response = json.loads(s.recv(1024))
+            if response['ok']:
+                break
+            if response['reason'] == 1:
+                print('Username is in use. Choose a different one')
+                username = get_username(new_user)
+        s.sendall(json.dumps({'action': 'login', 'username': username, 'password': password}).encode('utf-8'))
+        s.recv(1024)
+    else:
+        while True:
+            s.sendall(json.dumps({'action': 'login', 'username': username, 'password': password}).encode('utf-8'))
+            response = json.loads(s.recv(1024))
+            if response['ok']:
+                break
+            if response['reason'] == 1:
+                print('Incorrect username. Please reenter')
+                username = get_username(new_user)
+            elif response['reason'] == 2:
+                print('Incorrect password. Please reenter')
+                password = get_password(new_user)
+
     sending_thread = threading.Thread(target=send_messages, args=(s,), daemon=True)
     sending_thread.start()
     while True:
         try:
-            data = s.recv(1024)
+            received = s.recv(1024)
         except ConnectionResetError:
             print('Connection closed by server')
             break
-        print(data.decode('utf-8'))
+        received = json.loads(received)
+        if received['action'] == 'send':
+            print(f'{received["user"]}: {received["text"]}')
+        elif received['action'] == 'connection':
+            print(f'{received["user"]} connected')
+        elif received['action'] == 'disconnection':
+            print(f'{received["user"]} disconnected')
--- Server.py
+++ Server.py
@@ -1,45 +1,71 @@
 import socket
 import threading
+import json
 
 HOST = 'localhost'
 PORT = 56789
+AUTHENTICATION_PORT = 56788
 
 
-def manage_client(conn, iden):
-    username = conn.recv(1024).decode('utf-8')
-    users[iden]['name'] = username
-    print(f'{iden} is named {username}')
-    users[iden]['ready'] = True
-    disseminate_message(iden, f'{users[iden]["name"]} connected'.encode('utf-8'), prefix_username=False)
+def manage_client(conn, identity):
     with conn:
         while True:
             try:
                 data = conn.recv(1024)
-                if not data:
-                    break
-                print(f'Received {data} from {iden}')
-                disseminate_message(iden, data)
             except ConnectionResetError:
-                print(f'{iden} disconnected')
-                disseminate_message(
-                                    iden,
-                                    f'{users[iden]["name"]} disconnected'.encode('utf-8'),
-                                    prefix_username=False
-                                    )
-                del users[iden]
-                break
+                if users[identity]['logged_in']:
+                    print(f'{identity} disconnected')
+                    disseminate_message(identity, {'action': 'disconnection',
+                                                   'user': users[identity]['username']})
+                    del users[identity]
+                    break
             except BlockingIOError:
                 pass
 
+            else:
+                if not data:
+                    break
+                data = json.loads(data)
+                if data['action'] == 'send':
+                    if users[identity]['logged_in']:
+                        print(f'{users[identity]["username"]}: {data["text"]}')
+                        disseminate_message(identity, {'action': 'send',
+                                                       'text': data['text'],
+                                                       'user': users[identity]['username']})
+                elif data['action'] == 'login':
+                    if data['username'] not in logins:
+                        conn.sendall(json.dumps({'ok': False, 'reason': 1}).encode('utf-8'))
 
-def disseminate_message(origin, message, prefix_username=True):
-    if prefix_username:
-        to_send = f'{users[origin]["name"]}: {message.decode("utf-8")}'.encode('utf-8')
-    else:
-        to_send = message
-    for iden, data in users.items():
-        if iden != origin and data['ready']:
-            data['connection'].sendall(to_send)
+                    elif logins[data['username']] != data['password']:
+                        conn.sendall(json.dumps({'ok': False, 'reason': 2}).encode('utf-8'))
+
+                    else:
+                        conn.sendall(json.dumps({'ok': True}).encode('utf-8'))
+                        users[identity]['logged_in'] = True
+                        users[identity]['ready'] = True
+                        users[identity]['username'] = data['username']
+                        disseminate_message(identity, {'action': 'connection',
+                                                       'user': users[identity]['username']})
+
+                elif data['action'] == 'register':
+                    if data['username'] in logins:
+                        conn.sendall(json.dumps({'ok': False, 'reason': 1}).encode('utf-8'))
+                    else:
+                        conn.sendall(json.dumps({'ok': True}).encode('utf-8'))
+                        logins[data['username']] = data['password']
+
+                elif data['action'] == 'listen':
+                    users[identity]['ready'] = True
+
+                else:
+                    print(f'! Bad action {data} from {identity}')
+
+
+def disseminate_message(origin, data):
+    data = json.dumps(data).encode('utf-8')
+    for identity, user_data in users.items():
+        if identity != origin and user_data['ready']:
+            user_data['connection'].sendall(data)
 
 
 def accept_connections():
@@ -52,15 +78,16 @@ def accept_connections():
                 connection, address = s.accept()
             except BlockingIOError:
                 continue
-            identity = f'{address[0]}:{address[1]}'
-            print(f'Recieved connection from {identity}')
-            client_thread = threading.Thread(target=manage_client, args=(connection, identity), daemon=True)
-            users[identity] = {'connection': connection, 'thread': client_thread, 'ready': False}
+            identitytity = f'{address[0]}:{address[1]}'
+            print(f'Recieved connection from {identitytity}')
+            client_thread = threading.Thread(target=manage_client, args=(connection, identitytity), daemon=True)
+            users[identitytity] = {'connection': connection, 'thread': client_thread, 'ready': False, 'logged_in': False}
             threads.append(client_thread)
             client_thread.start()
 
 
 users = {}
+logins = {}
 threads = []
 running = True
 
